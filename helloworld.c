f5ceeeb2480bfbc64a40dc3b018d1a76b4325459
Gandhi666 <141110372+Gandhi666@users.noreply.github.com>
2023-08-01 10:51:38 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main(){
+int i;
+int n;
+printf("Enter how many times you want to display hello world: ");
+scanf("%d", &n);
+for(i=0;i<n;i++){
+printf("hello world\n");
+}
+return 0;
+};
+
